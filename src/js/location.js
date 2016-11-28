87b8b013234e327c5684a08d493291295b9f6efd
Tommy Gaessler <tommy.gaessler@gmail.com>
2016-11-27 23:29:15 -0700

    added more places

--- src/js/location.js
+++ src/js/location.js
@@ -42,7 +42,27 @@ var allLocations = [{
   place: 'Prague, Czech Republic',
   lat: '50.075893',
   long: '14.423730'
-}
+},
+// {
+//   place: 'Prague, Czech Republic',
+//   lat: '50.075893',
+//   long: '14.423730'
+// },
+// {
+//   place: 'Prague, Czech Republic',
+//   lat: '50.075893',
+//   long: '14.423730'
+// },
+// {
+//   place: 'Prague, Czech Republic',
+//   lat: '50.075893',
+//   long: '14.423730'
+// },
+// {
+//   place: 'Prague, Czech Republic',
+//   lat: '50.075893',
+//   long: '14.423730'
+// }
 ];
 
 function setLatLng (locations) {
